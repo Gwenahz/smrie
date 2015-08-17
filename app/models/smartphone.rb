ddcc7afe7609d5f658892fede50eecd84ddb96fb
Gwenael Ahoomey <ahoomey@gmail.com>
2015-08-17 17:28:17 +0200

    bug fix smartphones

--- app/models/smartphone.rb
+++ app/models/smartphone.rb
@@ -2,7 +2,7 @@ class Smartphone < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
-  validates :marque, :modele, :couleur, presence: true
+  validates :marque, :modele, presence: true
   validates_attachment_presence :image
   
   belongs_to :user
