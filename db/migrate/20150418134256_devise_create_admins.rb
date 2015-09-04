fbae52cb2e14d7ae658a1f41878be666d8df4477
Gwenael Ahoomey <ahoomey@gmail.com>
2015-09-04 15:25:32 +0200

    Bug Admin

--- db/migrate/20150418134256_devise_create_admins.rb
+++ db/migrate/20150418134256_devise_create_admins.rb
@@ -2,7 +2,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration
   def change
     create_table(:admins) do |t|
       
-      t.database_authenticatable :null => false
+      #t.database_authenticatable :null => false
       t.trackable
       t.lockable
             
