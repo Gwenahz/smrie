74597ec8a6770c46be04ee7651cf8f5e611fa569
Gwenael Ahoomey <ahoomey@gmail.com>
2015-10-19 01:15:16 +0200

    Mailer Help

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => "lasmartphonerie.com",
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
