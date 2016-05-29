f3396c7cc60c1dcca7a51c85ee266d55028cfd7b
Gwenael Ahoomey <ahoomey@gmail.com>
2016-05-29 18:01:43 +0200

    algo prix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -64,6 +64,17 @@ class PagesController < ApplicationController
 
   def devis
     @stockparam = params[:stock]
+    @majparam = params[:maj].to_i
+    @stock = Stock.find(@stockparam)
+    @stock_id = @stock.id
+    @id_smartphone = Stock.find(@stock_id).id_smartphone
+    @id_panne = Stock.find(@stock_id).id_panne
+    @smartphone = Smartphone.find(@id_smartphone)
+    @panne = Panne.find(@id_panne)
+  end
+
+  def diagnostic
+    @stockparam = params[:stock]
     @stock = Stock.find(@stockparam)
     @stock_id = @stock.id
     @id_smartphone = Stock.find(@stock_id).id_smartphone
